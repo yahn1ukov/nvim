97b9ff7538b8b0b8c5097a3fbf61e604c013d04c
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2022-12-16 20:14:47 +0200

    refactor: code review

--- after/plugin/dracula.rc.lua
+++ after/plugin/dracula.rc.lua
@@ -1,9 +1,7 @@
 local status, dracula = pcall(require, 'dracula')
 if (not status) then return end
 
-dracula.setup({
-    transparent_bg = true
-})
+dracula.setup()
 
 vim.cmd [[ colorscheme dracula ]]
 
--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -5,16 +5,12 @@ telescope.setup {
     defaults = {
         layout_config = {
             horizontal = {
-                height = 0.7,
                 prompt_position = "top",
-                width = 0.6
             }
         },
-        preview = false,
-        prompt_prefix = "  ",
+        prompt_prefix = "   ",
         selection_caret = "  ",
         file_ignore_patterns = { 'node_modules', '.git', 'dist', 'obj', 'bin' }
     },
     extensions = { file_browser = { hijack_netrw = true } }
 }
-
