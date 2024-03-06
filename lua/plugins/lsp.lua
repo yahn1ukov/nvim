a9adc1015e7f3f57a1d6fd5edee71cb765f64c56
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2024-03-06 23:10:14 +0200

    refactor(lsp.lua): vim.diagnostic.config has been modified

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,11 +30,10 @@ return {
 
     local capabilities = cmp_lsp.default_capabilities()
 
-    local signs = { Error = "󰅙", Warn = "", Hint = "󰌵", Info = "" }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    vim.diagnostic.config({
+      virtual_text = { spacing = 2 },
+      float = { header = "" },
+    })
 
     lspconfig.lua_ls.setup({
       on_attach = on_attach,
